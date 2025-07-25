7fa76942fbdce70d9748e1e1215820304cb056fe
Nikshan <nikshanps07@gmail.com>
2025-07-25 17:01:12 +0530

    fixed

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,11 +1,13 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",          
-  password: "nikshan",
-  host: "localhost",
+  host: process.env.POSTGRES_HOST || 'localhost',
+  user: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'nikshan',
+  database: process.env.POSTGRES_DB || 'Todo',
   port: 5432,
-  database: "Todo"
 });
 
 module.exports = pool;
+
+
